9451d3e53c0c822c99deb9a79a7c0871e6bd27fe
zeok <manolodehuevo@hotmail.com>
2016-02-05 16:16:49 +0100

    Modificacion y eliminacion
    Modificado el archivo App1.java para añadir metodo multiplicación
    Eliminado el archivo App2.java

--- MisMatematicas/src/main/java/es/egc/App1.java
+++ MisMatematicas/src/main/java/es/egc/App1.java
@@ -14,4 +14,11 @@ public class App1
 		log.info("Returning "+a+"/"+b+"="+result);
 		return result;
 	}
+
+	public static int multiplica(int a, int b){
+		int result= a*b;
+		log.info("Returning "+a+"*"+b+"="+result);
+		return result;
+	}
+
 }
